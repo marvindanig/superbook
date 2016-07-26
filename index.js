17293d9d553d0c006be2e995c0186ae66f2df2f1
Marvin Danig <marvin@marvindanig.com>
2016-07-26 13:38:12 -0400

    Experimenting on corkscrew functions

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-+!~-(function(w, d, undefined) {
++!~-((w, d, undefined) => {
 
   
 
-  let book = {};
+  // let book = {};
 
-  book.bounds = document.getElementById("book").getBoundingClientRect();
+  // book.bounds = document.getElementById("book").getBoundingClientRect();
 
-  console.log(book.bounds);
+  // console.log(book.bounds);
 
 })(this, document);
 
 
+// !+-~((undefined, ...a) => { })(this, document);
+
+
++!-~((...args) => { console.table(args); })(this, document);
 
 // Asynchronous recursion
 // (function loopsie(w, d) {
