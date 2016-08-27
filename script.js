cbdcf1e1308820aa4b4efeae3012679de63d006f
Marvin Danig <marvin@marvindanig.com>
2016-08-27 16:47:02 -0400

    Adding a test suite, adding matchMedia for mode selection of book

--- script.js
+++ script.js
@@ -2,19 +2,25 @@ import './css/style.css';
 
 import 'babel-polyfill';
 
-import $ from 'jquery';
+import isTouch from './lib/isTouch.js';
 
-import pages from './js/pages.js';
+import pages from './lib/pages.js';
 
-import graph from './js/graph.js';
+import graph from './lib/graph.js';
 
-
-
-!+-~(($, w, d, undefined) => {
-
-  console.log(pages);
+import mode from './lib/mode.js';
 
 
+!+-~((w, d, undefined) => {
 
+    let book = {};
 
-})($, window, document);
+    mode.onChange('all and (min-width: 870px)', function(match) {
+        book.mode = match ? 'double' : 'single';
+          console.table(this);
+
+    });
+
+    console.log(mode.getMatch('all and (min-width: 870px)'));
+
+})(window, document);
