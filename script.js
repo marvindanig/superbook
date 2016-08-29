d175c97e04f74d127a186554aa2911ea600f39ca
Marvin Danig <marvin@marvindanig.com>
2016-08-29 11:47:08 -0400

    Getting direction and a bit of a page stacking

--- script.js
+++ script.js
@@ -4,23 +4,109 @@ import 'babel-polyfill';
 
 import isTouch from './lib/isTouch.js';
 
-import pages from './lib/pages.js';
+import whatever from './lib/pages.js';
 
 import graph from './lib/graph.js';
 
-import mode from './lib/mode.js';
+import viewer from './lib/mode.js';
 
 
 !+-~((w, d, undefined) => {
 
+    let node = d.getElementById('book');
+
     let book = {};
 
-    mode.onChange('all and (min-width: 870px)', function(match) {
-        book.mode = match ? 'double' : 'single';
-          console.table(this);
+    book.mode = viewer.getMatch('all and (min-width: 870px)') ? 'double' : 'single';  
 
+    viewer.onChange('all and (min-width: 870px)', function(match) { 
+      book.mode = match ? 'double' : 'single'; 
+    }) ;
+
+
+
+
+    // We sweep pages here.
+    book.pages = [...node.children].map(function(obj) {
+        return wrapPage(obj);
     });
 
-    console.log(mode.getMatch('all and (min-width: 870px)'));
+
+    function wrapPage(pageObj) {
+        // Wrap appropriately
+        return pageObj;
+    }
+
+
+    removeChildren(node);
+
+    function removeChildren(node) {
+        while (node.hasChildNodes())
+            node.removeChild(node.lastChild);
+    }
+
+
+    // Print pages, add, remove into the DOM etc.
+    book.range = [1, 6];
+
+
+    function printRange(range) {
+        for (let i = range[0]; i <= range[1]; i++) {
+            let elem = book.pages[i - 1];
+            elem.id = i;
+            node.appendChild(elem);
+        }
+    }
+
+    printRange(book.range);
+
+    d.getElementById('next').addEventListener('click', () => {
+        turnPage('next');
+    });
+
+    d.getElementById('previous').addEventListener('click', () => {
+        turnPage('previous');
+    });
+
+
+    function turnPage(direction) {
+
+        switch (direction) {
+            case 'next':
+
+                removePage(book.range[0]);
+                addPage(book.range[1] + 1);
+                book.range[0]++;
+                book.range[1]++;
+                break;
+
+            case 'previous':
+
+                removePage(book.range[1]);
+                addPage(book.range[0] - 1);
+                book.range[0]--;
+                book.range[1]--;
+
+                break;
+
+        }
+
+        // updateView
+    }
+
+
+    function removePage(pageNumber) {
+        console.log('Remove ' + pageNumber);
+        node.removeChild(d.getElementById(pageNumber));
+    }
+
+    function addPage(pageNumber) {
+        console.log('Add ' + pageNumber);
+        let elem = book.pages[pageNumber - 1];
+        elem.id = pageNumber;
+        node.appendChild(elem);
+    }
+
+    console.table(book);
 
 })(window, document);
